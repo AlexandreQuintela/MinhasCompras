09b51a056a5bfc0881932dc10d9d771587d82bfe
alexandrequintela <alexsquin@gmail.com>
2024-08-12 22:05:22 -0300

    Lista de produtos

--- MinhasCompras/Views/ListaProduto.xaml.cs
+++ MinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,10 +1,23 @@
+using MinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
     public ListaProduto()
     {
         InitializeComponent();
+        lst_produtos.ItemsSource = lista;
+    }
+
+    // Sempre é chamado quando uma tela é chamada
+    protected async override void OnAppearing()
+    {
+        lista.Clear();
+        List<Produto> listaTemp = await App.Db.GetAll();
+        listaTemp.ForEach(x => lista.Add(x));
     }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -18,5 +31,25 @@ public partial class ListaProduto : ContentPage
             DisplayAlert("Ops", ex.Message, "OK");
             throw;
         }
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+        lista.Clear();
+        List<Produto> listaTemp = await App.Db.Search(q);
+        listaTemp.ForEach(x => lista.Add(x));
+    }
+
+    private void ToolbarItem_Somar(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(x => x.Total);
+        string msg = $"O total é {soma:C}";
+        DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Remover(object sender, EventArgs e)
+    {
+
     }
 }
