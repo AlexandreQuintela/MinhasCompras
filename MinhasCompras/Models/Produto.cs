c41b8ff68a26f1770d39bbef76ee2953db90260c
alexandrequintela <alexsquin@gmail.com>
2024-08-12 23:26:50 -0300

    tratamento com exceptions

--- MinhasCompras/Models/Produto.cs
+++ MinhasCompras/Models/Produto.cs
@@ -4,10 +4,40 @@ namespace MinhasCompras.Models;
 
 public class Produto
 {
+    string descricao;
+    double quantidade, preco;
+
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
-    public string Descricao { get; set; }
-    public double Quantidade { get; set; }
-    public double Preco { get; set; }
+    public string Descricao
+    {
+        get => descricao;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("A descrição não pode ser nula, vazia ou conter apenas espaços em branco.");
+            descricao = value;
+        }
+    }
+    public double Quantidade
+    {
+        get => quantidade;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("A quantidade não pode ser menor ou igual a zero.");
+            quantidade = value;
+        }
+    }
+    public double Preco
+    {
+        get => preco;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("O preço não pode ser menor ou igual a zero.");
+            preco = value;
+        }
+    }
     public double Total { get => Quantidade * Preco; }
 }
